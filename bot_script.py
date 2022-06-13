da6c347229bc9f9a1c8f2e4ba7759c96f8bcf66b
lisboamab <lisboamab@gmail.com>
2022-06-13 20:55:16 -0300

    bot finalizado

--- bot_script.py
+++ bot_script.py
@@ -1,3 +1,9 @@
 from selenium import webdriver
+from pyautogui import press
 
 navegador = webdriver.Chrome()
+
+navegador.get("https://dc-full-stack.github.io/Site-da-Turma/pages/modulo01.html")
+
+for _ in range(1000):
+    press('F5')
