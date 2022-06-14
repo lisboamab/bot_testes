d2e0320aec930da0555fd7e5a85f6bd1e621d8a3
lisboamab <lisboamab@gmail.com>
2022-06-13 21:35:16 -0300

    salvando correções

--- bot_script.py
+++ bot_script.py
@@ -1,13 +1,13 @@
 from selenium import webdriver
 from pyautogui import press
 
-navegador = webdriver.Chrome()
+numero_de_recargas = int(input("Digite o número de vezes que você quer recarregar: "))
 
 site = input("Digite o site desejado ")
 
-navegador.get(site)
+navegador = webdriver.Chrome()
 
-numero_de_recargas = int(input("Digite o número de vezes que você quer recarregar: "))
+navegador.get(site)
 
 for _ in range(numero_de_recargas):
     press('F5')
